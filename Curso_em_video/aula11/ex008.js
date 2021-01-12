fc71fbd12bea9eedd15c9e82f65f7c439ad5a292
Luciene Borges <Luciene Borges>
2021-01-12 16:54:48 -0300

    condiçoes simples

---
+++ Curso_em_video/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 78.2;
+
+console.log(`A velocidade d seu carro é ${vel}Km/h`);
+
+if (vel > 60) {
+  //condição simples
+  console.log('Limite de velocidade acima do permitido. MULTADO!!!');
+}
+
+console.log('Dirija com cinto de segurança');
---
+++ Curso_em_video/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil';
+
+if (pais == 'Brasil') {
+  console.log('vc é brasileiro');
+} else {
+  console.log('vc é estrangeiro');
+}
